94aea5655a0fdaf1f72f98bb7e0724c31721ccde
lucia <69209369+Lls28es@users.noreply.github.com>
2021-05-06 13:50:40 -0300

    front

---
+++ src/components/Admin/Admin.jsx
@@ -0,0 +1,27 @@
+/*eslint-disable*/
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import Dashboard from './Dashboard'
+import '../../scss/components/_admin.scss';
+import { getAllUsers, getAllOrders, getAllrRoles } from '../../redux/actions/actionBack'
+
+function Admin() {
+
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+ 
+    dispatch(getAllUsers());
+    dispatch(getAllOrders());
+    dispatch(getAllrRoles());
+
+  }, []);
+
+  return (
+    <div className="Admin">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default Admin;
